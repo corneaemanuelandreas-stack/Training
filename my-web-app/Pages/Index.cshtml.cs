9fbb49aa01198f4693a0aaa32394ae9da1e9dc16
Emanuel <cornea.emanuel.andreas@gmail.com>
2025-11-20 15:30:49 +0200

    more

---
+++ my-web-app/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace my_web_app.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
